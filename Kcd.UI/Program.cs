b177529d378711d16353dca7f9ab49dff13c379b
Ben Abdallah Oussama <benabdalla.oussama@gmail.com>
2024-09-05 15:46:19 +0200

    - Added appsettings.json to the kcd.UI project.

--- Kcd.UI/Program.cs
+++ Kcd.UI/Program.cs
@@ -13,11 +13,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+var apiUrl = builder.Configuration["ApiUrl"];
+
+
 builder.Services.AddTransient<JwtAuthorizationMessageHandler>();
 
 // Using configuration settings for HttpClient base addresses
 builder.Services.AddHttpClient();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7268/api/") }); //TODO: change this hardcoded api url
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 
 builder.Services.AddScoped<IAuthService, AuthService>();
 
